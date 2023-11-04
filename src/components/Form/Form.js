a4d66f3526bb7a9ad7687a8729cbaa4528099da0
darterss <darters@mail.ru>
2023-11-05 01:06:08 +0300

    init

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,7 +2,7 @@ import React, {useCallback, useEffect, useState} from 'react';
 import './Form.css'
 import {useTelegram} from "../../hooks/useTelegram";
 const Form = () => {
-    const [country, setCountry] = useState('null', 'null', 'null');
+    const [country, setCountry] = useState();
     const [street, setStreet] = useState();
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
