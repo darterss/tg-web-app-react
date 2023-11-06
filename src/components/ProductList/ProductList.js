200e191ff8ce5da209e98c8a89aa3f9f6b015112
darterss <darters@mail.ru>
2023-11-07 01:06:58 +0300

    init

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://192.168.0.23:3000/web-data', {
+        fetch('http://localhost:8000', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
